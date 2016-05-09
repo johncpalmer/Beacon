aa7f309cfb30feb3f9e9f4e93e5310d0222dbeed
John Palmer <john_palmer@brown.edu>
2016-05-09 10:33:24 -0400

    copied Jon's beacon function

--- www/js/HomeView.js
+++ www/js/HomeView.js
@@ -25,6 +25,36 @@ var HomeView = function (service) {
         });
     };
 
+     this.searchForBeacon = function() {
+      
+        console.log('searching');
+        var uuid = 'b9407f30-f5f8-466e-aff9-25556b57fe6d';
+        //identifier isn't actually a part of the beacon, but is sent back through the callbacks (so you know which triggered which event)
+        var identifier = "testBeacon";
+        var major = 46375;
+        var minor = 49433;
+        var beaconRegion = new cordova.plugins.locationManager.BeaconRegion(identifier, uuid, major, minor);
+          
+        var delegate = new cordova.plugins.locationManager.Delegate();
+          
+        //this is the function that we actually want for seeing whether or not the beacon is in range
+        delegate.didDetermineStateForRegion = function(pluginResult) {
+            console.log(JSON.stringify(pluginResult));
+        };
+        delegate.didStartMonitoringForRegion = function(pluginResult) {
+            console.log(JSON.stringify(pluginResult));
+        };
+          delegate.didRangeBeaconsInRegion = function(pluginResult) {
+            console.log(JSON.stringify(pluginResult));
+        };
+          
+        cordova.plugins.locationManager.setDelegate(delegate);
+        cordova.plugins.locationManager.requestAlwaysAuthorization();
+        cordova.plugins.locationManager.startMonitoringForRegion(beaconRegion).fail(function(e){}).done();
+      
+    };
+
     this.initialize();
     socket.emit('showMeRooms');
+    //this.searchForBeacon();
 }
